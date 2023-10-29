62375c3372198d43da33da9f3d7823e3bd7e5d3c
jetski.ci <gidrokisik@gmail.com>
2023-10-29 20:12:03 +0500

    fix bugs

--- index.js
+++ index.js
@@ -24,52 +24,58 @@ bot.command('start', async (ctx) => {
         .text('Случайный вопрос')
         .resized();
     await ctx.reply(
-        'Привет! Я - твой бот для подготовки к интервью 👨‍🎓',
+        'Привет! Я - Бот подготовки к Frontend интервью 🤖',
     );
     await ctx.reply('С чего начнем? Выбери тему вопроса в меню', {
-        reply_markup: startKeyboard
+        reply_markup: startKeyboard,
     });
 });
 
-bot.hears(['HTML', 'CSS', 'JavaScript', 'React', 'Случайный вопрос'], async (ctx) => {
-    const topic = ctx.message.text.toLowerCase();
-    const { question, questionTopic } = getRandomQuestion(topic);
+bot.hears(
+    ['HTML', 'CSS', 'JavaScript', 'React', 'Случайный вопрос'],
+    async (ctx) => {
+        const topic = ctx.message.text.toLowerCase();
+        const {
+            question,
+            questionTopic
+        } = getRandomQuestion(topic);
 
-    let inlineKeyboard;
+        let inlineKeyboard;
 
-    if (question.hasOptions) {
-        const buttonRows = question.options.map((option) => [
-            InlineKeyboard.text(
-                option.text,
+        if (question.hasOptions) {
+            const buttonRows = question.options.map((option) => [
+                InlineKeyboard.text(
+                    option.text,
+                    JSON.stringify({
+                        type: `${questionTopic}-option`,
+                        isCorrect: option.isCorrect,
+                        questionId: question.id,
+                    }),
+                ),
+            ]);
+
+            inlineKeyboard = InlineKeyboard.from(buttonRows);
+        } else {
+            inlineKeyboard = new InlineKeyboard().text(
+                'Узнать ответ',
                 JSON.stringify({
-                    type: `${questionTopic}-option`,
-                    isCorrect: option.isCorrect,
+                    type: questionTopic,
                     questionId: question.id,
                 }),
-            ),
-        ]);
+            );
+        }
 
-        inlineKeyboard = InlineKeyboard.from(buttonRows);
-    } else {
-        inlineKeyboard = new InlineKeyboard().text(
-            'Узнать ответ',
-            JSON.stringify({
-                type: questionTopic,
-                questionId: question.id,
-            }),
-        );
-    }
-
-    await ctx.reply(question.text, {
-        reply_markup: inlineKeyboard,
-    });
-})
+        await ctx.reply(question.text, {
+            reply_markup: inlineKeyboard,
+        });
+    },
+);
 
 bot.on('callback_query:data', async (ctx) => {
     const callbackData = JSON.parse(ctx.callbackQuery.data);
 
     if (!callbackData.type.includes('option')) {
-        const answer = getCorrectAnswer(callbackData.type, callbackData.duestionID);
+        const answer = getCorrectAnswer(callbackData.type, callbackData.questionId);
         await ctx.reply(answer, {
             parse_mode: 'HTML',
             disable_web_page_preview: true,
@@ -84,7 +90,10 @@ bot.on('callback_query:data', async (ctx) => {
         return;
     }
 
-    const answer = getCorrectAnswer(callbackData.type.split('-')[0], callbackData.questionId);
+    const answer = getCorrectAnswer(
+        callbackData.type.split('-')[0],
+        callbackData.questionId,
+    );
     await ctx.reply(`Неверно ❌ Правильный ответ: ${answer}`);
     await ctx.answerCallbackQuery();
 });
@@ -94,11 +103,11 @@ bot.catch((err) => {
     console.error(`Error while handling update ${ctx.update.update_id}:`);
     const e = err.error;
     if (e instanceof GrammyError) {
-        console.error("Error in request:", e.description);
+        console.error('Error in request:', e.description);
     } else if (e instanceof HttpError) {
-        console.error("Could not contact Telegram:", e);
+        console.error('Could not contact Telegram:', e);
     } else {
-        console.error("Unknown error:", e);
+        console.error('Unknown error:', e);
     }
 });
 
